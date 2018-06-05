9282f1e98aa1f52f028b0620d1bed7aa590586c1
Dhanapathi.Marepalli <dhanapathi.marepalli@gmail.com>
2018-06-05 16:14:04 +0530

    Binary Search

--- src/main/java/org/dhana/search/BinarySearch.java
+++ src/main/java/org/dhana/search/BinarySearch.java
@@ -1,5 +1,35 @@
 package org.dhana.search;
 
-public class BinarySearch {
+import java.util.stream.IntStream;
 
+public class BinarySearch {
+    public int search(int[] numbers, int x, int low, int high) {
+        if (high < low) {
+            return -1;
+        }
+
+        int mid = (low + high) / 2;
+
+        if (numbers[mid] == x) {
+            return mid;
+        }
+
+        if (numbers[mid] < x) {
+            return search(numbers, x, mid + 1, high);
+        } else {
+            return search(numbers, x, low, mid - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarySearch binarySearch = new BinarySearch();
+        int[] numbers = IntStream.of(2, 4, 6, 8, 10, 33, 34, 66).toArray();
+        int index = binarySearch.search(numbers, 33, 0, numbers.length -1);
+
+        if (index == -1) {
+            System.out.println("The number searched is not available.");
+        } else {
+            System.out.println("The number searched is available at index " + index);
+        }
+    }
 }
